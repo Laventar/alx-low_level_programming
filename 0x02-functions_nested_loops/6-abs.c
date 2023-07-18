ee558cef9565a4ec0d48f8fb41b55da744579922
Laventar <khabetsa16@gmail.com>
2023-07-18 09:25:47 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Prints absolute value of an integer
+ * @n: The integer to check
+ * Return: 0 Always success
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
