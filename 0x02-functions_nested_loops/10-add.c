108fd159aef1ef79d56638806b77cbb1da843868
Laventar <khabetsa16@gmail.com>
2023-07-18 11:38:32 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ * @m: integer being added
+ * @n: integer being added
+ * Return: 0 Always success
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
