22c1d30a3c6edd32beebfc4dc62c6d330de455ea
Laventar <khabetsa16@gmail.com>
2023-07-18 09:20:57 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Print sign of a number
+ * @n: The integer to check
+ * Return: 0 Always success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}
