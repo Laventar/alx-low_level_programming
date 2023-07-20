aac3b1e51e22364bbc82acf445682159fe67f15f
Laventar <khabetsa16@gmail.com>
2023-07-20 08:32:41 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Check for uppercase letter
+ * @x: The number to be checked
+ * Return: 1 for uppercase letter or else 0
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
